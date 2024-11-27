c0ebdf9c97d8194f585c7dcd787d26467935c0b2
filha243 <filha243@student.liu.se>
2024-11-27 10:32:16 +0100

    design changes

--- lab2/flutter_application_lab2/lib/views/home_page.dart
+++ lab2/flutter_application_lab2/lib/views/home_page.dart
@@ -25,10 +25,10 @@ class _HomePageState extends State<HomePage> {
   final TextEditingController cardCvvController = TextEditingController();
 
   final FlipCardController flipCardController = FlipCardController();
+  String cardBrand = "visa";
 
   @override
   Widget build(BuildContext context) {
-    String cardBrand = 'visa'; // Define a variable to store the card brand
     return Scaffold(
       body: SafeArea(
         child: SingleChildScrollView(
@@ -54,7 +54,7 @@ class _HomePageState extends State<HomePage> {
                       cardNumber: cardNumberController.text.isEmpty
                           ? "#### #### #### ####"
                           : cardNumberController.text,
-                      cardBrand: getCardBrand(cardNumberController.text), 
+                      cardBrand: cardBrand,
                     ),
                   ),
                   back: Padding(
@@ -67,8 +67,7 @@ class _HomePageState extends State<HomePage> {
                       ),
                       child: Container(
                         height: 230,
-                        padding: const EdgeInsets.only(
-                            bottom: 22.0),
+                        padding: const EdgeInsets.only(bottom: 22.0),
                         child: Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -82,133 +81,143 @@ class _HomePageState extends State<HomePage> {
                                 borderRadius: BorderRadius.circular(5),
                               ),
                             ),
-                          Padding(
-                            padding: const EdgeInsets.only(top: 8.0),
-                            child: Container(
-                           
-                              color: Colors.white, // Backaground color
-                              child: SizedBox(
-                                height: 35,
-                                width: 500,
-                                child: Align(
-                                  alignment: Alignment.centerRight,
-                                  child: Text(
-                                    cardCvvController.text.isEmpty ? "CVV  " : cardCvvController.text,
-                                    style: const TextStyle(
-                                      color: Colors.black,
-                                      fontSize: 10,
+                            Padding(
+                              padding: const EdgeInsets.only(top: 8.0),
+                              child: Container(
+                                color: Colors.white, // Backaground color
+                                child: SizedBox(
+                                  height: 35,
+                                  width: 500,
+                                  child: Align(
+                                    alignment: Alignment.centerRight,
+                                    child: Text(
+                                      cardCvvController.text.isEmpty
+                                          ? "CVV  "
+                                          : cardCvvController.text,
+                                      style: const TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 10,
+                                      ),
                                     ),
                                   ),
                                 ),
                               ),
                             ),
-                          ),
-
-
-                            
-                       const SizedBox(height: 10),
-                          Padding(
-                            padding: const EdgeInsets.all(16.0), // Adjust padding as needed
-                            child: Align(
-                              alignment: Alignment.centerRight, // Aligns the child to the right
-                              child: Image.asset(
-                              "assets/logos/$cardBrand.png", // Dynamic logo
-                              height: 60,
-                              width: 60,
+                            const SizedBox(height: 10),
+                            Padding(
+                              padding: const EdgeInsets.all(
+                                  16.0), // Adjust padding as needed
+                              child: Align(
+                                alignment: Alignment
+                                    .centerRight, // Aligns the child to the right
+                                child: Image.asset(
+                                  "assets/logos/$cardBrand.png", // Dynamic logo
+                                  height: 60,
+                                  width: 60,
+                                ),
+                              ),
                             ),
-                            ),
-                          ),
-
-          
                           ],
                         ),
                       ),
                     ),
                   )),
               const SizedBox(height: 40),
-              Container(
-                height: 55,
-                width: MediaQuery.of(context).size.width / 1.12,
-                decoration: BoxDecoration(
-                  color: Colors.grey[200],
-                  borderRadius: BorderRadius.circular(15),
-                ),
-                child: TextFormField(
-                  controller: cardNumberController,
-                  keyboardType: TextInputType.number,
-                  decoration: const InputDecoration(
-                    border: InputBorder.none,
-                    contentPadding:
-                        EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-                    hintText: 'Card Number',
-                    hintStyle: TextStyle(
-                      color: Colors.grey,
-                      fontSize: 16,
-                    ),
-                    prefixIcon: Icon(
-                      Icons.credit_card,
-                      color: Colors.grey,
-                    ),
-                  ),
-                  inputFormatters: [
-                    FilteringTextInputFormatter.digitsOnly,
-                    LengthLimitingTextInputFormatter(16),
-                    CardInputFormatter(),
-                  ],
-                  onChanged: (value) {
-                  var text = value.replaceAll(RegExp(r'\s+\b|\b\s'), ' ');
-                  setState(() {
-                    cardNumberController.value = cardNumberController.value.copyWith(
-                      text: text,
-                      selection: TextSelection.collapsed(offset: text.length),
-                      composing: TextRange.empty,
-                    );
-                    cardBrand = getCardBrand(value); // Set the card brand when card number changes
-                  });
-                },
-                ),
-              ),
-              const SizedBox(height: 12),
-              Container(
-                height: 55,
-                width: MediaQuery.of(context).size.width / 1.12,
-                decoration: BoxDecoration(
-                  color: Colors.grey[200],
-                  borderRadius: BorderRadius.circular(15),
-                ),
-                child: TextFormField(
-                  controller: cardHolderNameController,
-                  keyboardType: TextInputType.name,
-                  decoration: const InputDecoration(
-                    border: InputBorder.none,
-                    contentPadding:
-                        EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-                    hintText: 'Full Name',
-                    hintStyle: TextStyle(
-                      color: Colors.grey,
-                      fontSize: 16,
-                    ),
-                    prefixIcon: Icon(
-                      Icons.person,
-                      color: Colors.grey,
-                    ),
-                  ),
-                  onChanged: (value) {
-                    setState(() {
-                      cardHolderNameController.value =
-                          cardHolderNameController.value.copyWith(
-                              text: value,
-                              selection:
-                                  TextSelection.collapsed(offset: value.length),
-                              composing: TextRange.empty);
-                    });
-                  },
-                ),
-              ),
-              const SizedBox(height: 12),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
+                  // Card Number Field
+                  const Text(
+                    'Card Number',
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 14,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  const SizedBox(height: 5),
+                  Container(
+                    height: 55,
+                    width: MediaQuery.of(context).size.width / 1.12,
+                    decoration: BoxDecoration(
+                      color: Colors.grey[200],
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: TextFormField(
+                      controller: cardNumberController,
+                      keyboardType: TextInputType.number,
+                      decoration: const InputDecoration(
+                        border: InputBorder.none,
+                        contentPadding:
+                            EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                      ),
+                      inputFormatters: [
+                        FilteringTextInputFormatter.digitsOnly,
+                        LengthLimitingTextInputFormatter(16),
+                        CardInputFormatter(),
+                      ],
+                      onChanged: (value) {
+                        var text = value.replaceAll(RegExp(r'\s+\b|\b\s'), ' ');
+                        setState(() {
+                          cardNumberController.value =
+                              cardNumberController.value.copyWith(
+                            text: text,
+                            selection:
+                                TextSelection.collapsed(offset: text.length),
+                            composing: TextRange.empty,
+                          );
+                          cardBrand = getCardBrand(text);
+                        });
+                      },
+                    ),
+                  ),
+                  const SizedBox(height: 12),
+                  const Text(
+                    'Card Name',
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 14,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  const SizedBox(height: 5),
+                  Container(
+                    height: 55,
+                    width: MediaQuery.of(context).size.width / 1.12,
+                    decoration: BoxDecoration(
+                      color: Colors.grey[200],
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: TextFormField(
+                      controller: cardHolderNameController,
+                      keyboardType: TextInputType.name,
+                      decoration: const InputDecoration(
+                        border: InputBorder.none,
+                        contentPadding:
+                            EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                      ),
+                      onChanged: (value) {
+                        setState(() {
+                          cardHolderNameController.value =
+                              cardHolderNameController.value.copyWith(
+                                  text: value,
+                                  selection: TextSelection.collapsed(
+                                      offset: value.length),
+                                  composing: TextRange.empty);
+                        });
+                      },
+                    ),
+                  ),
+                  const SizedBox(height: 12),
+                  const Text(
+                    'Expiry Date',
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 14,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  const SizedBox(height: 5),
                   Container(
                     height: 55,
                     width: MediaQuery.of(context).size.width / 2.4,
@@ -223,15 +232,6 @@ class _HomePageState extends State<HomePage> {
                         border: InputBorder.none,
                         contentPadding:
                             EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-                        hintText: 'MM/YY',
-                        hintStyle: TextStyle(
-                          color: Colors.grey,
-                          fontSize: 16,
-                        ),
-                        prefixIcon: Icon(
-                          Icons.calendar_today,
-                          color: Colors.grey,
-                        ),
                       ),
                       inputFormatters: [
                         FilteringTextInputFormatter.digitsOnly,
@@ -252,53 +252,62 @@ class _HomePageState extends State<HomePage> {
                     ),
                   ),
                   const SizedBox(width: 14),
-                  Container(
-                    height: 55,
-                    width: MediaQuery.of(context).size.width / 2.4,
-                    decoration: BoxDecoration(
-                      color: Colors.grey[200],
-                      borderRadius: BorderRadius.circular(15),
-                    ),
-                    child: TextFormField(
-                      controller: cardCvvController,
-                      keyboardType: TextInputType.number,
-                      decoration: const InputDecoration(
-                        border: InputBorder.none,
-                        contentPadding:
-                            EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-                        hintText: 'CVV',
-                        hintStyle: TextStyle(
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      // CVV Label
+                      const Text(
+                        'CVV',
+                        style: TextStyle(
                           color: Colors.grey,
-                          fontSize: 16,
-                        ),
-                        prefixIcon: Icon(
-                          Icons.lock,
-                          color: Colors.grey,
+                          fontSize: 14,
+                          fontWeight: FontWeight.w500,
                         ),
                       ),
-                      inputFormatters: [
-                        FilteringTextInputFormatter.digitsOnly,
-                        LengthLimitingTextInputFormatter(3),
-                      ],
-                      onTap: () {
-                        setState(() {
-                          Future.delayed(const Duration(milliseconds: 300), () {
-                            flipCardController.toggleCard();
-                          });
-                        });
-                      },
-                      onChanged: (value) {
-                        setState(() {
-                          int length = value.length;
-                          if (length == 4 || length == 9 || length == 14) {
-                            cardNumberController.text = '$value ';
-                            cardNumberController.selection =
-                                TextSelection.fromPosition(
-                                    TextPosition(offset: value.length + 1));
-                          }
-                        });
-                      },
-                    ),
+                      const SizedBox(height: 5),
+                      // CVV Text Field
+                      Container(
+                        height: 55,
+                        width: MediaQuery.of(context).size.width / 2.4,
+                        decoration: BoxDecoration(
+                          color: Colors.grey[200],
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        child: TextFormField(
+                          controller: cardCvvController,
+                          keyboardType: TextInputType.number,
+                          decoration: const InputDecoration(
+                            border: InputBorder.none,
+                            contentPadding: EdgeInsets.symmetric(
+                                horizontal: 20, vertical: 15),
+                          ),
+                          inputFormatters: [
+                            FilteringTextInputFormatter.digitsOnly,
+                            LengthLimitingTextInputFormatter(3),
+                          ],
+                          onTap: () {
+                            setState(() {
+                              Future.delayed(const Duration(milliseconds: 300),
+                                  () {
+                                flipCardController.toggleCard();
+                              });
+                            });
+                          },
+                          onChanged: (value) {
+                            setState(() {
+                              int length = value.length;
+                              if (length == 4 || length == 9 || length == 14) {
+                                cardNumberController.text = '$value ';
+                                cardNumberController.selection =
+                                    TextSelection.fromPosition(
+                                  TextPosition(offset: value.length + 1),
+                                );
+                              }
+                            });
+                          },
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               ),
@@ -306,7 +315,7 @@ class _HomePageState extends State<HomePage> {
               ElevatedButton(
                 style: ElevatedButton.styleFrom(
                   foregroundColor: Colors.white,
-                  backgroundColor: Colors.deepPurpleAccent,
+                  backgroundColor: Colors.blue,
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(15),
                   ),
