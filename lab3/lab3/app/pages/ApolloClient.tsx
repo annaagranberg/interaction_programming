29febcb9cecfb527058df5942e52c352c28e06a2
Filip Hamrelius <91670593+hamreliusfilip@users.noreply.github.com>
2024-12-12 14:05:19 +0100

    Update ApolloClient.tsx

--- lab3/lab3/app/pages/ApolloClient.tsx
+++ lab3/lab3/app/pages/ApolloClient.tsx
@@ -4,7 +4,7 @@ const client = new ApolloClient({
   link: new HttpLink({
     uri: 'https://api.github.com/graphql', 
     headers: {
-      Authorization: `Bearer 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`, 
+      Authorization: ``, 
     },
   }),
   cache: new InMemoryCache(),
